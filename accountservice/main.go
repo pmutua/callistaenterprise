a9e6d7107e58bd73e814d8859933a2e00a388738
pmutua <example@live.com>
2019-02-20 10:33:31 +0300

    Import web server use it in main.go

--- accountservice/main.go
+++ accountservice/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/pmutua/callistaenterprise/goblog/accountservice/service"
+)
 
 var appName = "accountservice"
 
 func main() {
 	fmt.Printf("Start %v\n", appName)
+	service.StartWebServer("6767") //Hardcode port number
 }
